42a30b2a7818b8c36f987c45c002114c825a700b
JKY120-张艳龙 <zhangyanlong@wondersgroup.com>
2023-04-19 09:54:35 +0800

    奉贤 mysql语句转换为达梦SQL工具类

--- work-util/src/main/java/FengXianUtil.java
+++ work-util/src/main/java/FengXianUtil.java
@@ -19,79 +19,70 @@ public class FengXianUtil {
      */
     public static void filterNewAddField() {
 
-        String mysql_fields = "`id` varchar(32) NOT NULL,\n" +
-            "`username` varchar(32) NOT NULL COMMENT '用户名',\n" +
-            "`password` varchar(32) NOT NULL COMMENT '密码',\n" +
-            "`name` varchar(32) DEFAULT NULL COMMENT '姓名',\n" +
-            "`name_e` varchar(192) DEFAULT NULL,\n" +
-            "`name_m` varchar(64) DEFAULT NULL,\n" +
-            "`belong_branch_code` bigint(18) NOT NULL COMMENT '所属机构Id',\n" +
-            "`employee_no` varchar(16) DEFAULT NULL COMMENT '工号',\n" +
-            "`sex` tinyint(1) NOT NULL COMMENT 'GB/T 2261.1-2003性别代码：0=未知的性别；1=男性；2=女性； 9=未说明的性别',\n" +
-            "`birthday` char(10) DEFAULT NULL COMMENT '生日 2018-04-23',\n" +
-            "`email` varchar(64) DEFAULT NULL COMMENT '邮箱',\n" +
-            "`email_e` varchar(192) DEFAULT NULL,\n" +
-            "`email_m` varchar(64) DEFAULT NULL,\n" +
-            "`status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态：0=未启用状态；1=正常状态',\n" +
-            "`mobile` varchar(32) DEFAULT NULL COMMENT '手机号',\n" +
-            "`mobile_e` varchar(192) DEFAULT NULL,\n" +
-            "`mobile_m` varchar(64) DEFAULT NULL,\n" +
-            "`description` varchar(1024) DEFAULT NULL COMMENT '描述',\n" +
-            "`deleted` tinyint(1) DEFAULT '0' COMMENT '是否删除： 0=未删除；1=删除',\n" +
-            "`role_id` varchar(100) DEFAULT '0' COMMENT '角色id',\n" +
+        String mysql_fields = "`id` char(32) NOT NULL,\n" +
+            "`schedule_id` char(32) NOT NULL COMMENT '排班ID',\n" +
+            "`schedule_detail_id` char(32) NOT NULL COMMENT '排班详情ID',\n" +
+            "`hospital_id` char(32) NOT NULL COMMENT '医院ID',\n" +
+            "`doctor_id` char(32) NOT NULL COMMENT '医生ID',\n" +
+            "`doctor_name` varchar(128) NOT NULL COMMENT '医生姓名',\n" +
+            "`dept_id` char(32) DEFAULT NULL COMMENT '科室ID（一级）',\n" +
+            "`dept_code` varchar(16) NOT NULL COMMENT '科室编码',\n" +
+            "`dept_name` varchar(128) NOT NULL COMMENT '科室名称',\n" +
+            "`sub_dept_id` char(32) DEFAULT NULL COMMENT '二级科室ID',\n" +
+            "`sub_dept_code` varchar(16) NOT NULL COMMENT '二级科室编码 = 门诊收费代码',\n" +
+            "`sub_dept_name` varchar(128) NOT NULL COMMENT '二级科室名称',\n" +
+            "`schedule_date` date NOT NULL COMMENT '号源日期',\n" +
+            "`week_day` char(1) NOT NULL COMMENT '星期几(一、二、三、四、五、六、日)',\n" +
+            "`outpatient_charges_code` varchar(16) DEFAULT NULL COMMENT '门诊收费代码',\n" +
+            "`schedule_type` char(2) DEFAULT '1' COMMENT '门诊收费类型(号源类型)    1=普通 ,  2 = 专家,  3=专病',\n" +
+            "`schedule_start_time` char(8) NOT NULL COMMENT '号源开始时间 例：10:00',\n" +
+            "`schedule_end_time` char(8) NOT NULL COMMENT '号源结束时间 例：17:00',\n" +
+            "`total_count` int(11) NOT NULL DEFAULT '0' COMMENT '时段号源总量',\n" +
+            "`remain_count` int(11) NOT NULL DEFAULT '0' COMMENT '剩余号源数量',\n" +
+            "`order_count` int(11) NOT NULL DEFAULT '0' COMMENT '已约号源数量',\n" +
+            "`service_code` int(8) NOT NULL DEFAULT '0' COMMENT '10=在线复诊，20=在线咨询 ，30=核酸挂号，40=在线续方，50=远程门诊 60:全科咨询；70:预约挂号;',\n" +
+            "`service_price` decimal(10,2) DEFAULT NULL COMMENT '当前排班对应的服务价格',\n" +
+            "`resource_status` tinyint(4) DEFAULT '1' COMMENT '号源状态：1、正常；2.停诊',\n" +
+            "`deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除1=是，0=否',\n" +
+            "`is_enabled` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否可用1=是，0=否',\n" +
+            "`is_modify` tinyint(2) DEFAULT '1' COMMENT '1、正常；2.撤销。',\n" +
+            "`doctor_activity_id` int(8) DEFAULT NULL COMMENT '医生活动id',\n" +
             "`create_time` datetime NOT NULL COMMENT '创建时间',\n" +
-            "`create_uid` varchar(32) NOT NULL COMMENT '创建用户ID',\n" +
-            "`edit_time` datetime DEFAULT NULL COMMENT '更新时间',\n" +
-            "`edit_uid` varchar(32) DEFAULT NULL COMMENT '更新用户ID',\n" +
-            "`login_fail_times` int(2) DEFAULT NULL,\n" +
-            "`locked_time` datetime DEFAULT NULL,\n" +
-            "`lastest_login_time` datetime DEFAULT NULL COMMENT '最近一次登录时间',\n" +
-            "`login_times` int(11) DEFAULT '0' COMMENT '登录系统次数',\n" +
-            "`is_locked` tinyint(1) DEFAULT '0',\n" +
-            "`reset_password` tinyint(1) DEFAULT '1' COMMENT '是否需要重置密码：1=是，0=否',\n" +
-            "`first_set_password_time` datetime DEFAULT NULL COMMENT '首次设置密码时间',\n" +
-            "`set_email_time` datetime DEFAULT NULL COMMENT '设置邮箱时间',\n" +
-            "`email_verify_time` datetime DEFAULT NULL COMMENT '邮箱认证时间',\n" +
-            "`is_verify_email` tinyint(4) DEFAULT '0' COMMENT '邮箱是否验证（0、未验证   1、已验证）',\n" +
-            "`set_mobile_time` datetime DEFAULT NULL COMMENT '设置手机号码时间',\n" +
-            "`is_verify_mobile` tinyint(4) DEFAULT NULL COMMENT '手机是否验证（0、未验证   1、已验证）',\n" +
-            "`mobile_verify_time` datetime DEFAULT NULL COMMENT '手机号认证时间',\n" +
-            "`last_set_password_time` datetime DEFAULT NULL COMMENT '上次更新密码时间',\n" +
-            "`sensitive_data_password` varchar(32) DEFAULT NULL COMMENT '敏感信息密码',";
+            "`create_uid` char(32) NOT NULL COMMENT '创建用户ID',\n" +
+            "`update_time` datetime NOT NULL COMMENT '更新时间',\n" +
+            "`update_uid` char(32) NOT NULL COMMENT '更新用户ID',\n" +
+            "`doctor_name_m` varchar(128) DEFAULT NULL COMMENT '医生姓名',";
 
-        String dm_fields = "\"id\" VARCHAR2(32) NOT NULL,\n" +
-            "\"username\" VARCHAR2(32) NOT NULL,\n" +
-            "\"password\" VARCHAR2(32) NOT NULL,\n" +
-            "\"name\" VARCHAR2(32) NOT NULL,\n" +
-            "\"belong_branch_code\" BIGINT NOT NULL,\n" +
-            "\"employee_no\" VARCHAR2(16),\n" +
-            "\"sex\" TINYINT NOT NULL,\n" +
-            "\"birthday\" VARCHAR(10),\n" +
-            "\"email\" VARCHAR2(64),\n" +
-            "\"status\" TINYINT DEFAULT 1 NOT NULL,\n" +
-            "\"mobile\" VARCHAR2(32),\n" +
-            "\"description\" VARCHAR2(1024),\n" +
-            "\"deleted\" TINYINT DEFAULT 0,\n" +
-            "\"role_id\" VARCHAR2(100) DEFAULT '0',\n" +
+        String dm_fields = "\"id\" VARCHAR(32) NOT NULL,\n" +
+            "\"schedule_id\" VARCHAR(32) NOT NULL,\n" +
+            "\"schedule_detail_id\" VARCHAR(32) NOT NULL,\n" +
+            "\"hospital_id\" VARCHAR(32) NOT NULL,\n" +
+            "\"doctor_id\" VARCHAR(32) NOT NULL,\n" +
+            "\"doctor_name\" VARCHAR2(64) NOT NULL,\n" +
+            "\"dept_id\" VARCHAR(32),\n" +
+            "\"dept_code\" VARCHAR2(16) NOT NULL,\n" +
+            "\"dept_name\" VARCHAR2(128) NOT NULL,\n" +
+            "\"sub_dept_id\" VARCHAR(32),\n" +
+            "\"sub_dept_code\" VARCHAR2(16) NOT NULL,\n" +
+            "\"sub_dept_name\" VARCHAR2(128) NOT NULL,\n" +
+            "\"schedule_date\" DATE NOT NULL,\n" +
+            "\"week_day\" VARCHAR(1) NOT NULL,\n" +
+            "\"outpatient_charges_code\" VARCHAR2(16),\n" +
+            "\"schedule_type\" VARCHAR(2) DEFAULT '1',\n" +
+            "\"schedule_start_time\" VARCHAR(8) NOT NULL,\n" +
+            "\"schedule_end_time\" VARCHAR(8) NOT NULL,\n" +
+            "\"total_count\" INT DEFAULT 0 NOT NULL,\n" +
+            "\"remain_count\" INT DEFAULT 0 NOT NULL,\n" +
+            "\"order_count\" INT DEFAULT 0 NOT NULL,\n" +
+            "\"service_code\" INT DEFAULT 0 NOT NULL,\n" +
+            "\"deleted\" TINYINT DEFAULT 0 NOT NULL,\n" +
+            "\"is_enabled\" TINYINT DEFAULT 1 NOT NULL,\n" +
+            "\"is_modify\" TINYINT DEFAULT 1,\n" +
             "\"create_time\" TIMESTAMP NOT NULL,\n" +
-            "\"create_uid\" VARCHAR2(32) NOT NULL,\n" +
-            "\"edit_time\" TIMESTAMP,\n" +
-            "\"edit_uid\" VARCHAR2(32),\n" +
-            "\"login_fail_times\" INT,\n" +
-            "\"locked_time\" TIMESTAMP,\n" +
-            "\"lastest_login_time\" TIMESTAMP,\n" +
-            "\"login_times\" INT DEFAULT 0,\n" +
-            "\"is_locked\" TINYINT DEFAULT 0,\n" +
-            "\"reset_password\" TINYINT DEFAULT 1,\n" +
-            "\"first_set_password_time\" TIMESTAMP,\n" +
-            "\"set_email_time\" TIMESTAMP,\n" +
-            "\"email_verify_time\" TIMESTAMP,\n" +
-            "\"is_verify_email\" TINYINT DEFAULT 0,\n" +
-            "\"set_mobile_time\" TIMESTAMP,\n" +
-            "\"is_verify_mobile\" TINYINT,\n" +
-            "\"mobile_verify_time\" TIMESTAMP,\n" +
-            "\"last_set_password_time\" TIMESTAMP,\n" +
-            "\"sensitive_data_password\" TIMESTAMP,";
+            "\"create_uid\" VARCHAR(32) NOT NULL,\n" +
+            "\"update_time\" TIMESTAMP NOT NULL,\n" +
+            "\"update_uid\" VARCHAR(32) NOT NULL,\n" +
+            "\"service_price\" DEC,";
 
         List<String> mysqlFieldList = new ArrayList<>();
         List<String> dmFieldList = new ArrayList<>();
